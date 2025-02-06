6f579f2e6491ffbe2eb8b4303441536f896cb1f4
caglatopak <160266658+caglatopak@users.noreply.github.com>
2025-02-06 23:13:10 +0100

    cagla

---
+++ src/JavaMethodlariDemo.java
@@ -0,0 +1,28 @@
+public class JavaMethodlariDemo {
+    public static void main(String[] args) {
+
+        konsolaYazdir();
+        yasadigimSehir();
+
+    }
+
+    private static void konsolaYazdir(){
+
+        System.out.println("merhaba dunya");
+    }
+
+    private static void yasadigimSehir(){
+        System.out.println("yasadigim sehir = Goes ");
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
