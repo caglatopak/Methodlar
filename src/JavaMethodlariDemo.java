9e0ab9c10aab66e4503fd5a6b1bd0947405ea8af
caglatopak <160266658+caglatopak@users.noreply.github.com>
2025-02-07 15:28:00 +0100

    7-2-2025

--- src/JavaMethodlariDemo.java
+++ src/JavaMethodlariDemo.java
@@ -1,6 +1,22 @@
 public class JavaMethodlariDemo {
     public static void main(String[] args) {
 
+        // access modifiers ( erisim belirleyiciler)
+        // public private protected default
+        // public : herkese acik kullanilabilir. tum paketler ve tum siniflar kullanabilir
+        // private:yalnizca ayni siniftan erisilebilir en kisitlayicidir encapsulation in cekirdegini olusturur
+        // default: ersim belierleyici kullanilmadiginda default olarak belirlernir. sadece ayni java paketi icinde gorulur ve erisilir
+        // protected: ayni paket ve kendi sinifinin tum alt siniflarindan erisim saglanir
+
+        // public>protected>default>private
+
+        // methodlar cagirildiginda icinde bahsedilen belirli eylemleri gerceklestiren bir kod blogudur
+        // methodlar sadece cagrildiklarinda yurutulur.
+
+        // void ozel bir donus yapoasini istemiyorsak kullanilir\
+        // static bu method icin nesne olusturulmasina gerek olmadigini gosterir
+        //
+
         konsolaYazdir();
         yasadigimSehir();
 
