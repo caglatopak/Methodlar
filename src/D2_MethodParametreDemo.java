48c8b804b16510599ec694df0865d61eda50968f
caglatopak <160266658+caglatopak@users.noreply.github.com>
2025-02-07 16:33:50 +0100

    lastcommit

--- src/D2_MethodParametreDemo.java
+++ src/D2_MethodParametreDemo.java
@@ -6,6 +6,26 @@ public class D2_MethodParametreDemo {
         toplamaIslemi(5,10);
         toplamaIslemi(128,654);
         caglaTopak("cagla", 100);
+
+        System.out.println("--------------------------------------------------");
+
+       // int carpmaSonucu= carpma(5,9);
+        //System.out.println(carpmaSonucu);
+
+        System.out.println(carpma(5,9));
+        System.out.println(carpma(6,20));
+
+        System.out.println("-------------------------------------------------");
+
+
+        System.out.println(toplama(12,36));
+
+        System.out.println("----------------------------------------------------");
+
+        System.out.println(toplama(23,48,65 ));
+        System.out.println(toplama(1.2,5.6,7.8));
+
+
     }
 
     private static void konsolCiktisi(String isim ){
@@ -21,9 +41,47 @@ public class D2_MethodParametreDemo {
   // konsola kisinin adini ve sinav notunu yazdiran bir method olusturun
     // method 2 adet parametre alsin
 
+    // methodlari dinamik hale getirmek icin bu parametreler kullanilir
+
     private static void caglaTopak(String isim, int Not){
 
         System.out.println(isim + " " + Not );
     }
+
+    //return type geri donus veren methodlar
+
+    private static int carpma(int a, int b){
+        int carpmasonucu = a*b;
+        return carpmasonucu;
+
+
+    }
+
+     private static int toplama(int x, int y){
+
+        int toplamaIslemi= x+y;
+        return toplamaIslemi;
+        // yada sadece return x+y; de diyebiliriz.
+
+
+     }
+     // method overloading
+    // methodlarin asiri yuklenmesi
+    // method isimleri ayni olmasina ragmen parametreleri farkli oldugu icin java bunu farkeder ve hata vermez
+
+
+    private static int toplama(int x, int y, int z) {
+        return x + y + z;
+
+    }
+
+  private static double toplama(double a, double b, double c){
+
+        return a+b+c;
+
+  }
+
+
+
 
 }
---
+++ src/D3_Oop.java
@@ -0,0 +1,24 @@
+public class D3_Oop {
+    public static void main(String[] args) {
+
+        // nesne yonelimle programlama
+        // object orianted programming
+
+        // gercek dunya nesnelerini hem veri hem de kod iceren yazoilim nesneleeri olarak modellemenin bir yoludur.
+        // siniflar(class) ve nesneler (object) temel bilesenidir.
+        // class-based programlama olarak da gecer.
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
